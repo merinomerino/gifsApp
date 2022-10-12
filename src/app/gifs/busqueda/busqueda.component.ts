b16ce93ba0eb1ca45f6d43f204a9276a1e8d3c39
Your Name <you@example.com>
2022-10-11 21:01:35 -0500

    Service

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gif.service';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
+  
 
-  constructor() { }
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  
+
+  buscar(){
+    const valor : string = this.txtBuscar.nativeElement.value;
+    //console.log(valor)
+    this.gifsService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value='';
   }
+  constructor( private gifsService : GifsService) { }
+
+
 
 }
