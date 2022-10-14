d5709beb19460d1f5d65d0cd1cd6b66901fa7c88
Your Name <you@example.com>
2022-10-13 21:06:25 -0500

    muestra resultados
    consume api key
    y muestra las imágenes en el componente de resultados

--- src/app/gifs/resultados/resultados.component.ts
+++ src/app/gifs/resultados/resultados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gif.service';
 
 @Component({
   selector: 'app-resultados',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gifsService : GifsService) { }
+
+  get resultados(){
+    return this.gifsService.resultados;
+  }
 
   ngOnInit(): void {
   }
--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,19 +6,36 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   private _historial:string[]=[];
+  private apikey :string = 'ynXTYR5wsA4KGybm5ZXyAJshxuVk5Eu1';
+  public resultados : any [] = [];
 
   get historial(){
     return [...this._historial];
   }
+
+  constructor( private http : HttpClient){}
 
   buscarGifs( query : string){
     query= query.trim().toLocaleLowerCase();
     if(!this._historial.includes( query)){
       this._historial.unshift( query);
       this._historial = this._historial.splice(0,10);
+
+      // fetch('https://api.giphy.com/v1/gifs/search?api_key=ynXTYR5wsA4KGybm5ZXyAJshxuVk5Eu1&q=Goku&limit=10')
+      //     .then(resp => {
+      //       resp.json().then(data =>{
+      //         console.log(data);
+      //       })
+      //     })
+
+      this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=ynXTYR5wsA4KGybm5ZXyAJshxuVk5Eu1&q=${ query }&limit=10`)
+          .subscribe((resp : any) => {
+            console.log(resp.data)
+            this.resultados = resp.data;
+          })
     }
     console.log(this._historial)
   }
 
-  constructor() { }
+
 }
